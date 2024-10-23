9a066b89c7154cc441869e03e345f895b6214260
ZakPorat <zakporat2023@gmail.com>
2024-10-23 15:25:41 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
+
+// Import OHLC data from AlphaVantage API key
+
+// Format OHLC data
+
+// Backtest SMA indicator using OHLC data
